a6a6bdb773575f14ed117b1082130ce0edd0164a
the-27 <blackoficial2025@gmail.com>
2025-06-15 10:16:27 -0500

    「 ⚽ 𝐈𝐒𝐀𝐆𝐈 - 𝐘𝐎𝐈𝐂𝐇𝐈 🌴 」

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -1,7 +1,7 @@
 const handler = async (m, { isOwner, isAdmin, conn, text, participants, args, command, usedPrefix }) => {
   if (usedPrefix == 'a' || usedPrefix == 'A') return;
 
-  const customEmoji = global.db.data.chats[m.chat]?.customEmoji || '🔥';
+  const customEmoji = global.db.data.chats[m.chat]?.customEmoji || '🌲';
   m.react(customEmoji);
 
   if (!(isAdmin || isOwner)) {
@@ -22,7 +22,7 @@ const handler = async (m, { isOwner, isAdmin, conn, text, participants, args, co
   }
   teks += `╰══⌬\n> ${dev}`;
 
-  conn.sendMessage(m.chat, { text: teks, mentions: participants.map((a) => a.id) });
+  conn.sendMessage(m.chat, { text: teks, fkontak, mentions: participants.map((a) => a.id) });
 };
 
 handler.help = ['todos *<mensaje opcional>*'];
