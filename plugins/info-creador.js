7a5878f67e61fb5909a3af09ca7f7b5cf2175a5f
the-27 <blackoficial2025@gmail.com>
2025-06-15 07:37:21 -0500

    「 ⚽ 𝐈𝐒𝐀𝐆𝐈 - 𝐘𝐎𝐈𝐂𝐇𝐈 🌴 」

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,9 +1,12 @@
+import { promisify } from 'util';
+const delay = promisify(setTimeout);
+
 function handler(m, { conn }) {
   const suittag = "51969214380";
   const ownerNumber = suittag + "@s.whatsapp.net";
   const name = 'BLACK.OFC';
   const packname = '✦⃟⛧┋ ➪ _ISAGI ⛧ YOICHI_ ⚽┋⃟✧';
-  const dev = '© Modified by:  ꧁𓊈𒆜𝖙𝖍𝖊•𝒃𝒍𝒂𝒄𝒌𒆜𓊉꧂';
+  const dev = '© Modified by: ꧁𓊈𒆜𝖙𝖍𝖊•𝒃𝒍𝒂𝒄𝒌𒆜𓊉꧂';
   const channel = 'https://whatsapp.com/channel/0029Vb6BDQc0lwgsDN1GJ31i';
   const banner = 'https://files.catbox.moe/pp7ncd.jpg';
 
@@ -13,18 +16,21 @@ N:${name};;;;
 FN:${name}
 ORG:BLACK
 TEL;type=CELL;type=VOICE;waid=${suittag}:${suittag}
+EMAIL:blackoficial2025@gmail.com
+ADR:;;Lima;;;Peru
 END:VCARD`;
 
-  
   conn.sendMessage(m.chat, {
     text: `📌 *Información de contacto*\n\n👾 *Link:* wa.link/uowz07\n👤 *Nombre:* ${name}\n📞 *WhatsApp:* +${suittag}\n🔗 *Canal:* [Click aquí](${channel})`,
     footer: dev,
     buttons: [
-      { buttonId: `.status`, buttonText: { displayText: "🌐 ESTADO - BOT" }, type: 1 },
+      { buttonId: `.status`, buttonText: { displayText: "🌐 ESTADO - BOT" }, type: 1 }
     ],
     headerType: 1
   }, { quoted: m });
 
+  
+  await delay(1500);
 
   conn.sendMessage(m.chat, {
     contacts: {
