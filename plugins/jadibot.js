000d8a9f7043100a3956b721fe0654e61ce3f80d
the-27 <blackoficial2025@gmail.com>
2025-06-15 11:54:54 -0500

    「 ⚽ 𝐈𝐒𝐀𝐆𝐈 - 𝐘𝐎𝐈𝐂𝐇𝐈 🌴 」

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -81,11 +81,13 @@ const replyMessage = message.length === 0 ? `No hay Sub-Bots disponible.` : mess
 const totalUsers = users.length;
 const responseMessage = `
 ╭═━⬣ 𝐒𝐔𝐁𝐁𝐎𝐓𝐒 𝐉𝐀𝐃𝐈𝐁𝐎𝐓 🌹
-┃ 🏔️ 𝕆ℕ𝕃𝕀ℕ𝔼: *${totalUsers ||'0'}*
+┃ 🌴 𝕆ℕ𝕃𝕀ℕ𝔼: *${totalUsers ||'0'}*
 ╰═━━━━━━━━━━━━━━━━⬣\n
 ${replyMessage.trim()}\n═══════◆◈◆═══════`.trim();
 
-await _envio.sendFile(m.chat, img, 'jadibot.jpg', responseMessage, m, null, fake, fkontak, false, { mentions: _envio.parseMention(responseMessage) })
+await _envio.sendFile(m.chat, img, 'jadibot.jpg', responseMessage, m, null, fake, {}, fkontak, false, { mentions: _envio.parseMention(responseMessage) });
+
+//await _envio.sendFile(m.chat, img, 'jadibot.jpg', responseMessage, m, null, fake, fkontak, false, { mentions: _envio.parseMention(responseMessage) })
 break   
 }}
 
