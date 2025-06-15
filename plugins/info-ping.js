80fc4001aa5107ecf249b3ca5c82028b8eb0c86a
the-27 <blackoficial2025@gmail.com>
2025-06-14 19:04:57 -0500

    「 ⚽ 𝐈𝐒𝐀𝐆𝐈 - 𝐘𝐎𝐈𝐂𝐇𝐈 🌴 」

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -1,3 +1,4 @@
+/*
 import speed from 'performance-now'
 import { spawn, exec, execSync } from 'child_process'
 
@@ -16,5 +17,37 @@ conn.sendMessage(m.chat, { text: result, edit: sentMsg.key }, { quoted: m })
 handler.help = ['ping']
 handler.tags = ['info']
 handler.command = ['ping', 'p']
+
+export default handler
+*/
+
+
+import speed from 'performance-now'
+import { exec } from 'child_process'
+
+let handler = async (m, { conn }) => {
+  let timestamp = speed();
+  let latensi = speed() - timestamp;
+
+  exec(`neofetch --stdout`, (error, stdout, stderr) => {
+    let sysinfo = stdout.toString("utf-8").replace(/Memory:/, "Ram:");
+
+    const ping = `
+╭━━━⊰ 👾 *Isagi-Yoichi* ⚽ ⊱━━━╮
+┃ 🍂 *Estado:* ¡Activo ! 
+┃ 🕒 *Velocidad:* ${latensi.toFixed(4)} ms
+┃ 📊 *Sistema:*
+┃ ${sysinfo.split('\n').slice(1, 7).join('\n┃ ')}
+╰━━━━━━━━━━━━━━━━━━━━━━━╯
+`.trim();
+
+    conn.reply(m.chat, ping, fkontak, rcanal);
+  });
+}
+
+handler.help = ['ping']
+handler.tags = ['info']
+handler.command = ['ping', 'p']
+handler.register = true
 
 export default handler
